7a3efecb1434c1b7f8eb4b14308a0587aae19a88
Saul Zamora <saul.zamora@tagwizz.com.mx>
2023-08-30 11:11:52 -0600

    fix: missing function

--- back/src/models/employee.py
+++ back/src/models/employee.py
@@ -3,7 +3,6 @@ from .base import db
 
 
 class Employee(db.Model):
-
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     second_name = db.Column(db.String(200))
@@ -14,3 +13,16 @@ class Employee(db.Model):
     created_at = db.Column(db.DateTime(timezone=True), default=datetime.datetime.utcnow)
     updated_at = db.Column(db.DateTime(timezone=True), default=None, onupdate=datetime.datetime.utcnow)
 
+    def obj_to_dict(self):  # for build json format
+        return {
+            "id": self.id,
+            "name": self.name,
+            "price": self.price,
+            "second_name": self.second_name,
+            "emergency_contact": self.emergency_contact,
+            "emergency_phone": self.emergency_phone,
+            "blood_type": self.blood_type,
+            "position": self.position,
+            "updated_at": str(self.updated_at),
+            "created_at": str(self.created_at)
+        }
