b3b8dd4726f599a522c3283d7282affb01f075a2
Saul Zamora <saul.zamora@tagwizz.com.mx>
2023-08-30 01:27:42 -0600

    refactor(create_app): separate in functions

--- back/src/__init__.py
+++ back/src/__init__.py
@@ -1,34 +1,24 @@
-import os, json
+import json
 
 from flask import Flask, Response
+from .config.config import Config
+
+app = Flask(__name__, instance_relative_config=True)
 
 
 def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+    get_config()
 
     # a simple page that says hello
     @app.route('/')
     def hello():
         return Response(response=json.dumps({'msg': 'RH Api running', 'code': 200}),
-                 status=200,
-                 mimetype='application/json')
+                        status=200,
+                        mimetype='application/json')
 
     return app
+
+
+def get_config():
+    app.config.from_object(Config)
+
