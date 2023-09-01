28aeb23e3096b6e79589125b1d2a841b7c14e17b
Saul Zamora <saul.zamora@tagwizz.com.mx>
2023-09-01 11:29:51 -0600

    feat: secure history routes

--- back/src/routes/history.py
+++ back/src/routes/history.py
@@ -1,14 +1,24 @@
 from ..controllers.history import HistoryController
 from flask import Blueprint, request
+from ..utils.security import Security
 
 controller = HistoryController()
 history = Blueprint('history', __name__, url_prefix='/api/history')
+security = Security()
+
+
+@history.before_request
+def before_request_func():
+    if 'Authorization' not in request.headers.keys():
+        return controller.custom_error(code=401, msg='Unauthorized')
+
+    authorization = request.headers['Authorization']
+    token = authorization.split(" ")[1]
+
+    if not security.verify_jwt(token):
+        return controller.custom_error(code=401, msg='Unauthorized')
 
 
 @history.get('/employee/<int:employee>')
-def register(employee:int):
-
+def register(employee: int):
     return controller.get_employee_history(employee_id=employee)
-
-
-
